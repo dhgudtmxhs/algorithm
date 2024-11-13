fd46025ff2810a9b2f9c77bf1dcc4b0598909b24
Oh Hyoung Seok <126877943+dhgudtmxhs@users.noreply.github.com>
2024-11-13 18:21:21 +0900

    clone commit - 1113

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import solutions.problems1.ComboMasterIMS;
 import solutions.problems1.HashMarathon;
 import solutions.problems1.StackReverseWord;
 
@@ -5,14 +6,25 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) {
 
-        //test.testMain();
-        String result = HashMarathon.marathon(new String[]{"test1", "test2"}, new String[]{"test1"});
-        System.out.println(result);
-        StackReverseWord.reverseWords();
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            //test.testMain();
+            //String result = HashMarathon.marathon(new String[]{"test1", "test2"}, new String[]{"test1"});
+            //System.out.println(result);
+            //StackReverseWord.reverseWords();
+            ComboMasterIMS.combo();
+        } catch(Exception e) {
+            logger.log(Level.SEVERE, "An IOException occurred", e);
+
+        }
 
         //clone test
     }
